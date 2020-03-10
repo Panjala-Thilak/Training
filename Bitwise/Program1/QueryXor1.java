2466a93138b583f081c89937e6fa5a5c97bfafa9
Panjala-Thilak <54017470+Panjala-Thilak@users.noreply.github.com>
2020-03-10 16:39:23 +0530

    Update QueryXor1.java

--- Bitwise/Program1/QueryXor1.java
+++ Bitwise/Program1/QueryXor1.java
@@ -59,3 +59,71 @@ public class QueryXor1 {
 	}
 
 }
+================================================================================================================================================
+Modified Code
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int calculate(int[] a,int[][] store,int l,int r)
+    {
+        int ans=0,k;
+         if(store[l][r]==0)
+       {
+	       for(k=r;k>=l;k--)
+	       {
+		if(store[l][k]!=0)
+		{
+			break;
+		}
+		else
+			ans=ans^a[k];
+	       }
+	       if(k>=0)
+	       	ans^=store[l][k];
+	       store[l][r]=ans;
+	       return ans;
+       }
+       else
+       		return store[l][r];
+    }
+
+public static void main(String[] args) {      
+	
+Scanner scanner=new Scanner(System.in);
+   System.out.println("Enter the size of the Array");
+   int n=scanner.nextInt();
+   int[] a=new int[n];
+   int[][] store=new int[n][n];  
+   for (int[] row : store)
+   {
+            Arrays.fill(row,0);  
+        }
+   for(int i=0;i<n;i++)
+   {
+       a[i]=scanner.nextInt();
+   }
+   System.out.println("Enter the number of quries");
+   int q=scanner.nextInt();
+   int[][] query=new int[q][2];
+   int[] result=new int[q];
+   for(int i=0;i<q;i++)
+   {
+       query[i][0]=scanner.nextInt();
+       query[i][1]=scanner.nextInt();
+   }
+   for(int i=0;i<q;i++)
+   {
+    int k,flag=0;
+       result[i]=0;//a^0=a i.e.,any value XOR’d with zero is left unchanged
+       int l=query[i][0];
+       int r=query[i][1];
+       result[i]=calculate(a,store,l,r);
+   }
+   System.out.println();
+   System.out.println("Result is:");
+   for(int i=0;i<q;i++)
+   System.out.print(result[i]+" ");
+
+}
+}
