6db3f06ce065cdc3e60d6ef63663ba226dcb8ab0
mentor2.suntek <mentor2.suntek@gmail.com>
2020-03-10 10:26:13 +0530

    added review comments

--- Bitwise/Program4/MinNumberofReplacements.java
+++ Bitwise/Program4/MinNumberofReplacements.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class MinNumberofReplacements {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {                  // Code will give wrong ans for larger inputs
 		Scanner scanner=new Scanner(System.in);
 		int input=scanner.nextInt();
 		int input1=input;
-		int countminus=0;
+		int countminus=0;		//Use camelCase
 		int countplus=0;
 		/*If we consider n=n-1 if n is odd*/
 		while(input!=1)
@@ -24,7 +24,8 @@ public class MinNumberofReplacements {
 			countminus++;
 		}
 		}
-		/*If we consider n=n+1 if n is odd*/
+		/*If we consider n=n+1 if n is odd*/	// Try to do this for every iteration in else at line 21.
+							//there can be a combination of n+1 and n-1 to get the minimum replacements count
 		while(input1!=1)
 		{
 		if(input1%2==0)
