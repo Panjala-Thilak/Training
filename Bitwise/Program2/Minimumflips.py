c4c9b77e4c76529818b7b2507a850d7a3523b793
Panjala-Thilak <54017470+Panjala-Thilak@users.noreply.github.com>
2020-03-12 22:11:32 +0530

    Update Minimumflips.py

--- Bitwise/Program2/Minimumflips.py
+++ Bitwise/Program2/Minimumflips.py
@@ -1,14 +1,20 @@
 a=int(input())
 b=int(input())
 c=int(input())
-sum=bin(a+b)
+max_ab=bin(a) if len(bin(a))>len(bin(b)) else bin(b)
 bin_c=bin(c)
-max_length=len(sum)-2 if len(sum)>len(bin_c) else len(bin_c)-2
-new_bin_c=format(c,'0'+str(max_length)+'b')
-new_sum=format(a+b,'0'+str(max_length)+'b')
+max_length=len(max_ab)-2 if len(max_ab)>len(bin_c) else len(bin_c)-2
+a_bin=format(a,'0'+str(max_length)+'b')
+b_bin=format(b,'0'+str(max_length)+'b')
+c_bin=format(c,'0'+str(max_length)+'b')
 count=0
 for i in range(max_length):
-    if(new_sum[i]!=new_bin_c[i]):
-        count+=1
-
+    if(c_bin[i]=='1'):
+        if(a_bin[i]=='0' and b_bin[i]=='0'):
+            count+=1
+    else:
+        if(a_bin[i]=='1'):
+            count+=1
+        if(b_bin[i]=='1'):
+            count+=1
 print(count)
