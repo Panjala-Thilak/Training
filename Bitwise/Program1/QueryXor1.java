3b69791356de7b67f9666c05aa6bbc5c10f14cba
Panjala-Thilak <54017470+Panjala-Thilak@users.noreply.github.com>
2020-03-13 23:03:00 +0530

    resolved review comments

--- Bitwise/Program1/QueryXor1.java
+++ Bitwise/Program1/QueryXor1.java
@@ -4,68 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class QueryXor1 {
-
-	public static void main(String[] args) {       // this code wont work. And use functions instead of writing the whole code in main function.
-		// TODO Auto-generated method stub
-		Scanner scanner=new Scanner(System.in);
-	    System.out.println("Enter the size of the Array");
-	    int n=scanner.nextInt();
-	    int[] a=new int[n];
-	    int[][] mat=new int[n][n];  // change variable name to a more appropriate one
-	    for (int[] row : mat) 
-            Arrays.fill(row,0);         // use identation and curly braces
-	    for(int i=0;i<n;i++)
-	    {
-	        a[i]=scanner.nextInt();
-	    }
-	    System.out.println("Enter the number of quries");
-	    int q=scanner.nextInt();
-	    int[] result=new int[q]; 	// this shud be an array of arrays. Read the question again.
-	    for(int i=0;i<q;i++)
-	    {
-	    	int k,flag=0;
-	        result[i]=0;//a^0=a i.e.,any value XOR’d with zero is left unchanged
-	        int l=scanner.nextInt();
-	        int r=scanner.nextInt();
-	        if(mat[l][r]==0)
-	        {
-	        for(k=r-1;k>0;k--)
-	        {
-	        	if(mat[l][k]!=0)
-	        	{
-	        		flag=1;
-	        		break;
-	        	}
-	        	else
-	        		continue;
-	        }
-	        result[i]=mat[l][k];
-	        if(flag!=1)
-	        	k=l-1;
-	        for(int j=k+1;j<=r;j++)
-	        {
-	            result[i]=result[i]^a[j];
-	        }
-	        mat[l][r]=result[i];
-	        }
-	        else					
-	        	result[i]=mat[l][r];	//not computing anything if mat[l][r] == 0
-	    }
-	    System.out.println();
-	    System.out.println("Result is:");
-	    for(int i=0;i<q;i++)
-	    System.out.print(result[i]+" ");
-
-	}
-
-}
-================================================================================================================================================
-Modified Code
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
-    static int calculate(int[] a,int[][] store,int l,int r)
+    int calculate(int[] a,int[][] store,int l,int r)
     {
         int ans=0,k;
          if(store[l][r]==0)
@@ -90,7 +29,8 @@ public class Main {
 
 public static void main(String[] args) {      
 	
-Scanner scanner=new Scanner(System.in);
+	QueryXor1 run=new QueryXor1();
+   Scanner scanner=new Scanner(System.in);
    System.out.println("Enter the size of the Array");
    int n=scanner.nextInt();
    int[] a=new int[n];
@@ -98,7 +38,7 @@ Scanner scanner=new Scanner(System.in);
    for (int[] row : store)
    {
             Arrays.fill(row,0);  
-        }
+   }
    for(int i=0;i<n;i++)
    {
        a[i]=scanner.nextInt();
@@ -118,7 +58,7 @@ Scanner scanner=new Scanner(System.in);
        result[i]=0;//a^0=a i.e.,any value XOR’d with zero is left unchanged
        int l=query[i][0];
        int r=query[i][1];
-       result[i]=calculate(a,store,l,r);
+       result[i]=run.calculate(a,store,l,r);
    }
    System.out.println();
    System.out.println("Result is:");
