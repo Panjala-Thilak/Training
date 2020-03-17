15e6b11c09e6af2da3939c9cd5ab59917fb86b2e
Panjala-Thilak <54017470+Panjala-Thilak@users.noreply.github.com>
2020-03-18 00:02:07 +0530

    Resolved Review Comments

--- Week2/P3-Frequency/Frequency.py
+++ Week2/P3-Frequency/Frequency.py
@@ -1,8 +1,34 @@
+def binarySearchLeftMost(array,low,high,k):
+    global index
+    mid=(low+high)//2
+    if(low<=high):
+        if(array[mid]==k):
+            index=mid
+        if(array[mid]>=k):
+            return binarySearchLeftMost(array,low,mid-1,k)
+        else:
+            return binarySearchLeftMost(array,mid+1,high,k)
+    return index
+
+def binarySearchRightMost(array,low,high,k):
+    global index
+    mid=(low+high)//2
+    if(low<=high):
+        if(array[mid]==k):
+            index=mid
+        if(array[mid]>k):
+            return binarySearchRightMost(array,low,mid-1,k)
+        else:
+            return binarySearchRightMost(array,mid+1,high,k)
+    return index
+
+index=-1
 array=list(map(int,input().split(" ")))
 k=int(input())
-count=0
-for i in range(len(array)):
-    if array[i]==k:
-        count+=1
-
-print(count)
+array.sort()
+left=binarySearchLeftMost(array,0,len(array)-1,k) #finding the leftmost occurrence of k
+right=binarySearchRightMost(array,0,len(array)-1,k) ##finding the rightmost occurrence of k
+if(left==-1 and right==-1): #if k is not found in array
+    print('0')
+else:
+    print(right-left+1)
